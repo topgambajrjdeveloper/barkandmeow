8e7136b32bee124453991064c3b944bf1f98b71c
topgambajrjdeveloper <topgambjrjdeveloper@gmail.com>
2025-03-12 16:25:00 +0100

    prisma generate &&  next build

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || "https://barkandmeow.com", // Actualizado con el nuevo nombre
+    siteUrl: process.env.NEXT_PUBLIC_APP_URL || process.env.NEXT_PUBLIC_APP_URL, // Actualizado con el nuevo nombre
     generateRobotsTxt: true,
     exclude: ["/server-sitemap.xml"],
+    outDir: '.next',
     robotsTxtOptions: {
       additionalSitemaps: [
-        "https://barkandmeow.com/server-sitemap.xml", // Actualizado con el nuevo nombre
+        // "https://barkandmeow.com/server-sitemap.xml",
+        `https://${NEXT_PUBLIC_APP_URL}/server-sitemap.xml`
       ],
     },
   }
