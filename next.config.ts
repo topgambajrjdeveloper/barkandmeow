1cd40fc68409f0232c270f069d7779a5e3557930
topgambajrjdeveloper <topgambjrjdeveloper@gmail.com>
2025-03-12 05:57:22 +0100

    env-product

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
-    // !! WARN !!
-    // This will allow production builds to complete even with TypeScript errors
-    // Only use temporarily while fixing actual issues
-    // !! WARN !!
     ignoreBuildErrors: true,
   },
   distDir: 'build',
